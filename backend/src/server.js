0c142844417763b4ca65f23e3ac7b88b9c9e2b32
judoittt <your_email@example.com>
2026-03-01 16:31:22 +0700

    prepare delpoy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import tasksRoute from "./routes/tasks.route.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -10,13 +11,23 @@ const app = express();
 dotenv.config();
 // Port
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
-
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 // Convert Json
 app.use(express.json());
 // Tasks Route
 app.use("/api/tasks", tasksRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
